aea0d202e14415fedbb07a92982f642b5eea5a3a
mostafa salah <memosalah553@gmail.com>
2026-06-03 20:33:50 +0300

    feat: implement CRUD routes for products and stores

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const productController = require("../controllers/productController");
+const protect = require("../middlewares/authMiddleware");
 
 // Create a new product
-router.post("/", productController.createProduct);
+router.post("/", protect, productController.createProduct);
 
 // Get all products (supports ?store_id=&category=&is_active=&try_on_enabled=)
-router.get("/", productController.getProducts);
+router.get("/", protect, productController.getProducts);
 
 // Get a product by ID
-router.get("/:id", productController.getProductById);
+router.get("/:id", protect, productController.getProductById);
 
 // Update a product by ID
-router.put("/:id", productController.updateProduct);
+router.put("/:id", protect, productController.updateProduct);
 
 // Delete a product by ID
-router.delete("/:id", productController.deleteProduct);
+router.delete("/:id", protect, productController.deleteProduct);
 
 module.exports = router;
--- src/routes/storeRoutes.js
+++ src/routes/storeRoutes.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const storeController = require("../controllers/storeController");
+const protect = require("../middlewares/authMiddleware");
 
 // Create a new store
-router.post("/", storeController.createStore);
+router.post("/", protect, storeController.createStore);
 
 // Get all stores
-router.get("/", storeController.getStores);
+router.get("/", protect, storeController.getStores);
 
 // Get a store by ID
-router.get("/:id", storeController.getStoreById);
+router.get("/:id", protect, storeController.getStoreById);
 
 // Update a store by ID
-router.put("/:id", storeController.updateStore);
+router.put("/:id", protect, storeController.updateStore);
 
 // Delete a store by ID
-router.delete("/:id", storeController.deleteStore);
+router.delete("/:id", protect, storeController.deleteStore);
 
 module.exports = router;
