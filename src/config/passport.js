68928d605f905ebb58b14db65908db9df91f5c26
Kariman Ayman Abd El Latif <karimanaymankaram@gmail.com>
2026-06-14 23:40:22 +0300

    Update passport.js

--- src/config/passport.js
+++ src/config/passport.js
@@ -22,7 +22,7 @@ if (process.env.GOOGLE_CLIENT_ID) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "/api/auth/google/callback",
+        callbackURL: `${process.env.BASE_URL}/api/auth/google/callback`,
       },
 
       async (accessToken, refreshToken, profile, done) => {
